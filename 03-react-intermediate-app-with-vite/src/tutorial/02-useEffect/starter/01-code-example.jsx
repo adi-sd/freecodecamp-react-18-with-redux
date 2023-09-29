4d6702d280a1a44ea2f9c86aad888174ec71c500
Aditya Dhage <adhage93@gmail.com>
2023-09-29 12:24:29 -0700

    Completed React UseEffects

--- 03-react-intermediate-app-with-vite/src/tutorial/02-useEffect/starter/01-code-example.jsx
+++ 03-react-intermediate-app-with-vite/src/tutorial/02-useEffect/starter/01-code-example.jsx
@@ -1,15 +1,23 @@
-import { useState } from 'react';
+import { useState } from "react";
+
+let count = 0;
 
 const CodeExample = () => {
-  const [value, setValue] = useState(0);
+    const [value, setValue] = useState(0);
 
-  return (
-    <div>
-      <h1>value : {value}</h1>
-      <button className='btn' onClick={() => setValue(value + 1)}>
-        click me
-      </button>
-    </div>
-  );
+    const sayHello = () => {
+        console.log(`Hello There! ${count++}`);
+    };
+
+    sayHello();
+
+    return (
+        <div>
+            <h1>value : {value}</h1>
+            <button className="btn" onClick={() => setValue(value + 1)}>
+                click me
+            </button>
+        </div>
+    );
 };
 export default CodeExample;
